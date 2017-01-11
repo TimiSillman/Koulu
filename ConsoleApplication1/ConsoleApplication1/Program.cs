0fafc54a05b9dca0093353fa4da5f2d17b1c3080
K8353 <K8353@ICT-421-WS-36>
2017-01-11 15:00:48 +0200

    TOIMII

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,31 +9,37 @@ namespace ConsoleApplication1
         {
             int[] Numerot = new int[7];
             int[] VoittoNumerot = new int[7];
+            string j;
             Random rnd = new Random();
 
-            for (int i = 0; i < VoittoNumerot.Length; i++)
+            j = (Console.ReadLine());
+            for (int x = 0; x < Int32.Parse(j); x++)
             {
-                Numerot[i] = 0;
-            }
 
-            for (int i = 0; i < Numerot.Length; i++)
-            {
-                Numerot[i] = rnd.Next(1, 41);
-                if (VoittoNumerot.Contains(Numerot[i]))
+
+                for (int i = 0; i < VoittoNumerot.Length; i++)
                 {
-                    i--;
+                    Numerot[i] = 0;
                 }
-                else VoittoNumerot[i] = Numerot[i];
 
+                for (int i = 0; i < Numerot.Length; i++)
+                {
+                    Numerot[i] = rnd.Next(1, 41);
+                    if (VoittoNumerot.Contains(Numerot[i]))
+                    {
+                        i--;
+                    }
+                    else VoittoNumerot[i] = Numerot[i];
+
+                }
+                Array.Sort(VoittoNumerot);
+                for (int i = 0; i < VoittoNumerot.Length; i++)
+                {
+
+                    Console.Write(VoittoNumerot[i] + " ");
+                }
+                Console.WriteLine();
             }
-            Array.Sort(VoittoNumerot);
-            for (int i = 0; i < VoittoNumerot.Length; i++)
-            {
-
-                Console.Write(VoittoNumerot[i] + " ");
-            }
-            Console.WriteLine();
-
         }
 
     }
