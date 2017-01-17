41752462ad5498fa90353a673d2b74d99e903ee7
eks dee <eks dee@DESKTOP-S7MPV62>
2017-01-17 19:52:28 +0200

    ju

---
+++ ConsoleApplication1/Labra3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Labra3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testaus();
+        }
+        static void testaus()
+        {
+            Kiuas kiuas = new Kiuas();
+            kiuas.onOff = true;
+            kiuas.lämpötila = 90;
+            kiuas.Kosteus = 20;
+            Console.WriteLine("Kiuas on päällä: " + kiuas.onOff);
+            Console.WriteLine("Kiukaan lämpötila: " + kiuas.lämpötila);
+            Console.WriteLine("Kosteus: " + kiuas.Kosteus);
+
+        }
+    }
+}
---
+++ ConsoleApplication1/Labra3/kiuas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Labra3
+{
+    class Kiuas
+    {
+        public bool onOff { get; set; }
+        public float lämpötila { get; set; }
+        float kosteus;
+
+        public float Kosteus
+        {
+            get { return kosteus; }
+            set
+            {
+                kosteus = value;
+
+                if (kosteus < 0 || kosteus > 100)
+                {
+                    kosteus = 0;
+                }
+            }
+        }
+    }
+}
